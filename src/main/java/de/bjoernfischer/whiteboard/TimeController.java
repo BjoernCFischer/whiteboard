12ec0189829360ced5ce7930d8530d8f4b32c3df
Björn Fischer <bjoern.fischer@adesso.de>
2018-10-30 16:53:46 +0100

    Added TimeController to have a time service over SSE

---
+++ src/main/java/de/bjoernfischer/whiteboard/TimeController.java
@@ -0,0 +1,31 @@
+package de.bjoernfischer.whiteboard;
+
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+@RestController
+public class TimeController {
+
+    @GetMapping("/")
+    public Mono<String> root() {
+        return Mono.just("Hello World!");
+    }
+
+    @GetMapping("/time")
+    public Flux<ServerSentEvent<String>> getTime() {
+        return Flux.interval(Duration.ofSeconds(5))
+            .map(data -> ServerSentEvent.<String>builder()
+                .event("time")
+                .id(data.toString())
+                .data(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
+                .build());
+    }
+}
