31ec489ac01a8d1530ba0fa6d11f6db6b00def05
Björn Fischer <bjoern.fischer@adesso.de>
2018-11-23 13:03:07 +0100

    Added gangoffour endpoint to HelloWorldController

--- src/main/java/de/bjoernfischer/whiteboard/HelloWorldController.java
+++ src/main/java/de/bjoernfischer/whiteboard/HelloWorldController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.stream.Stream;
+
 @RestController()
 @RequestMapping("/hello")
 public class HelloWorldController {
@@ -23,6 +25,8 @@ public class HelloWorldController {
 
     @GetMapping("/gangoffour")
     public Flux<String> helloGoF() {
-        return Flux.just("Hello Erich Gamma!\n", "Hello Richard Helm!\n", "Hello Ralph Johnson!\n", "Hello John Vlissides!\n");
+        return Flux
+            .just("Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides")
+            .map(guy -> String.format("Hello %s!\n", guy));
     }
 }
