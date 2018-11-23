070bc796bfb5e5fb00ff180f33426e46a721f2a3
Björn Fischer <bjoern.fischer@adesso.de>
2018-11-23 12:41:36 +0100

    Added HelloWorldController.

---
+++ src/main/java/de/bjoernfischer/whiteboard/HelloWorldController.java
@@ -0,0 +1,20 @@
+package de.bjoernfischer.whiteboard;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController("/hello")
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public Mono<String> helloWorld() {
+        return Mono.just("Hello World!");
+    }
+
+    @GetMapping("/{name}")
+    public Mono<String> helloSomeone(@PathVariable String name) {
+        return Mono.just("Hello World!");
+    }
+}
