733a7bd3426acdae44bfc0b0648946980da0c77d
Björn Fischer <bjoern.fischer@adesso.de>
2018-11-23 12:51:35 +0100

    Fixed mapping Structure

--- src/main/java/de/bjoernfischer/whiteboard/HelloWorldController.java
+++ src/main/java/de/bjoernfischer/whiteboard/HelloWorldController.java
@@ -2,10 +2,12 @@ package de.bjoernfischer.whiteboard;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@RestController("/hello")
+@RestController()
+@RequestMapping("/hello")
 public class HelloWorldController {
 
     @GetMapping()
